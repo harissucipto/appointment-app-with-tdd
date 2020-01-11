11662301f75382d55a7b67d3bf897d585fc29081
haris sucipto <harisssucipto@gmail.com>
2020-01-11 23:14:15 +0700

    render each appointment in an li

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -58,6 +58,18 @@ describe('AppointmentsDayView', () => {
     ).toHaveLength(2);
   })
 
+  it('renders each appointment in an li', () => {
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0)},
+    { startsAt: today.setHours(13, 0)},
+    ];
+     render(<AppointmentsDayView appointments={appointments} />);
+     expect(container.querySelectorAll('li')).toHaveLength(2);
+     expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00')
+     expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00')
+  })
+
   
 
 });
