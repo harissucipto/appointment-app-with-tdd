241e6c62fe8306849b9d209d224df77dcced70a2
haris sucipto <harisssucipto@gmail.com>
2020-01-11 22:48:52 +0700

    pass appointmentsDayView

--- src/Appointment.js
+++ src/Appointment.js
@@ -4,6 +4,12 @@ export const Appointment = ({ customer: { firstName } }) => {
   return <div>{firstName}</div>;
 };
 
-export const AppointmentsDayView = () => (
-  <div id="appointmentsDayView"></div>
+export const AppointmentsDayView = ({ appointments }) => (
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map(appointment => (
+        <div key={appointment.startsAt} />
+      ))}
+    </ol>
+  </div>
 );
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -41,8 +41,23 @@ describe('AppointmentsDayView', () => {
   const render = component => ReactDOM.render(component, container);
 
   it('renders a div with the right id', () => {
-    render(<AppointmentsDayView appointment={[]} />);
+    render(<AppointmentsDayView appointments={[]} />);
     expect(container.querySelector('div#appointmentsDayView')).not.toBeNull()
   })
+
+  it('renders multiple appointments in an ol element', () => {
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0)},
+      { startsAt: today.setHours(13, 0)}
+    ]
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelector('ol')).not.toBeNull();
+    expect(
+      container.querySelector('ol').children
+    ).toHaveLength(2);
+  })
+
+  
 
 });
