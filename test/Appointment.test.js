a48c93074773c3992ade837c370f67386bfaca07
haris sucipto <harisssucipto@gmail.com>
2020-01-11 14:45:04 +0700

    first test

---
+++ test/Appointment.test.js
@@ -0,0 +1,7 @@
+describe('Appointment', () => {
+  it('renders the customer first name', () => {});
+
+  it('renders the customer first name', () => {
+    expect(document.body.textContent).toMatch('Ashley');
+  });
+});
