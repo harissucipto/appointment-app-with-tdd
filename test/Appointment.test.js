69cf9dfffe51299b582cde4754278aa15428cebe
haris sucipto <harisssucipto@gmail.com>
2020-01-11 21:56:34 +0700

    refactor test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -3,24 +3,31 @@ import ReactDOM from 'react-dom';
 import { Appointment } from '../src/Appointment';
 
 describe('Appointment', () => {
+  let container;
+  let customer;
+
+  const render = component =>
+    ReactDOM.render(component, container);
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
   it('renders the customer first name', () => {
-    const customer = { firstName: 'Ashley' };
-    const container = document.createElement('div');
-    ReactDOM.render(
-      <Appointment customer={customer} />,
-      container
-    );
+    // 3 A
+
+    // Arrange (Setup test dependencies)
+    customer = { firstName: 'Ashley' };
+    // Act: Executes production code under test
+    render(<Appointment customer={customer} />);
     // yang penting body punya text dengna string yang dibawah tidak peduli berapa level
+    // Assert: Check expectattaions are met
     expect(container.textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
-    const customer = { firstName: 'Jordan' };
-    const container = document.createElement('div');
-    ReactDOM.render(
-      <Appointment customer={customer} />,
-      container
-    );
+    customer = { firstName: 'Jordan' };
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Jordan');
   });
 });
