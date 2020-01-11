cd3580ffd189c6e2357ccc5321a3b29f994810df
haris sucipto <harisssucipto@gmail.com>
2020-01-11 21:26:36 +0700

    passing test 1

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,7 +1,15 @@
-describe('Appointment', () => {
-  it('renders the customer first name', () => {});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
 
+describe('Appointment', () => {
   it('renders the customer first name', () => {
+    const customer = { firstName: 'Ashley' };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    ReactDOM.render(component, container);
+    // yang penting body punya text dengna string yang dibawah tidak peduli berapa level
     expect(document.body.textContent).toMatch('Ashley');
   });
 });
