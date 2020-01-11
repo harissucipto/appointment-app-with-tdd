794bd1db36343688401313b3bea7330980e0c3f2
haris sucipto <harisssucipto@gmail.com>
2020-01-11 22:34:19 +0700

    pass test

--- src/Appointment.js
+++ src/Appointment.js
@@ -3,3 +3,7 @@ import React from 'react';
 export const Appointment = ({ customer: { firstName } }) => {
   return <div>{firstName}</div>;
 };
+
+export const AppointmentsDayView = () => (
+  <div id="appointmentsDayView"></div>
+);
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 describe('Appointment', () => {
   let container;
@@ -30,4 +30,19 @@ describe('Appointment', () => {
     render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Jordan');
   });
+});
+
+
+describe('AppointmentsDayView', () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement('div');
+  })
+  const render = component => ReactDOM.render(component, container);
+
+  it('renders a div with the right id', () => {
+    render(<AppointmentsDayView appointment={[]} />);
+    expect(container.querySelector('div#appointmentsDayView')).not.toBeNull()
+  })
+
 });
