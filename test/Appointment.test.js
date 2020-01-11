eff67e02d2a0244ce67fb6877c8b70640047507d
haris sucipto <harisssucipto@gmail.com>
2020-01-11 23:22:41 +0700

    show no appointment today

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment, AppointmentsDayView } from '../src/Appointment';
+import {
+  Appointment,
+  AppointmentsDayView
+} from '../src/Appointment';
 
 describe('Appointment', () => {
   let container;
@@ -32,44 +35,52 @@ describe('Appointment', () => {
   });
 });
 
-
 describe('AppointmentsDayView', () => {
   let container;
   beforeEach(() => {
     container = document.createElement('div');
-  })
-  const render = component => ReactDOM.render(component, container);
+  });
+  const render = component =>
+    ReactDOM.render(component, container);
 
   it('renders a div with the right id', () => {
     render(<AppointmentsDayView appointments={[]} />);
-    expect(container.querySelector('div#appointmentsDayView')).not.toBeNull()
-  })
+    expect(
+      container.querySelector('div#appointmentsDayView')
+    ).not.toBeNull();
+  });
 
   it('renders multiple appointments in an ol element', () => {
     const today = new Date();
     const appointments = [
-      { startsAt: today.setHours(12, 0)},
-      { startsAt: today.setHours(13, 0)}
-    ]
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) }
+    ];
     render(<AppointmentsDayView appointments={appointments} />);
     expect(container.querySelector('ol')).not.toBeNull();
-    expect(
-      container.querySelector('ol').children
-    ).toHaveLength(2);
-  })
+    expect(container.querySelector('ol').children).toHaveLength(2);
+  });
 
   it('renders each appointment in an li', () => {
     const today = new Date();
     const appointments = [
-      { startsAt: today.setHours(12, 0)},
-    { startsAt: today.setHours(13, 0)},
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) }
     ];
-     render(<AppointmentsDayView appointments={appointments} />);
-     expect(container.querySelectorAll('li')).toHaveLength(2);
-     expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00')
-     expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00')
-  })
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(
+      container.querySelectorAll('li')[0].textContent
+    ).toEqual('12:00');
+    expect(
+      container.querySelectorAll('li')[1].textContent
+    ).toEqual('13:00');
+  });
 
-  
-
+  it('initially shows a message saying there are no appointments tody', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.textContent).toMatch(
+      'There are no appointments scheduled for today.'
+    );
+  });
 });
